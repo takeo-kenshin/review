71ea22d7cebfb630379a295f3a521f67977e09f1
”竹尾憲真” <viento3009@gmail.com>
2020-06-06 13:06:53 +0000

    laravelmixの環境構築まで

---
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ReviewController extends Controller
+{
+    //
+    public function add(){
+        return view('admin.review.create');
+    }
+}
---
+++ resources/views/admin/review/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+@section('title','reviews new')
+@section('content')
+    <div class='container'>
+        <div class='row'>
+            <div class='col-md-8 mx-auto'>
+                <h2>review new</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
